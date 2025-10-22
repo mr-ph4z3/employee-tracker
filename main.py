085e4051c4e4dd3287744722dfca3fbc96d5375f
mr-ph4z3 <d.reinhofer@gmail.com>
2025-10-22 13:19:57 -0700

    main window created

---
+++ main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.title("Employee Management System")
+root.geometry(f"{int(root.winfo_screenwidth() / 2)}x{int(root.winfo_screenheight() / 2)}")
+
+home_content = Frame(root)
+
+root.mainloop()
